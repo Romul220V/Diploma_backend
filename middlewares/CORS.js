087d2560963bd98397c0641fc211f739d266fa93
Roman <zadoyaromain@yandex.ru>
2021-02-01 20:00:00 +0300

    fourth commit

--- middlewares/CORS.js
+++ middlewares/CORS.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
   // res.set('Access-Control-Allow-Origin', 'http://localhost:8080');
-  res.set('Access-Control-Allow-Origin', 'https://romul220v.github.io/Diploma_frontend');
+  res.set('Access-Control-Allow-Origin', 'https://romul220v.github.io');
   res.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE');
   res.set('Access-Control-Allow-Headers', 'Content-Type, credentials, Authorization');
   res.set('Access-Control-Allow-Credentials', 'true');
